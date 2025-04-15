0e3b9a1fde19a8f8649b301274736e85886e608b
Grace Mugoiri <gracemugoiri@gmail.com>
2025-04-15 21:13:47 +0300

    add all CRUD functions

--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,55 @@
 from flask import Blueprint, request, jsonify
-from .models import Episodes
-from . import db
+from app.models import db, Episode
 
-main = Blueprint('main', __name__)
-
-@main.route("/episodes", methods=["GET"])
-def get_episodes():
-    episodes = Episodes.query.all()
-    return jsonify([{
-        "id": e.id,
-        "title": e.title,
-        "description": e.description,
-        "audio_url": e.audio_url,
-        "date_posted": e.date_posted
-    } for e in episodes])
+main = Blueprint("main", __name__)
 
 @main.route("/episodes", methods=["POST"])
-def add_episode():
+def create_episode():
     data = request.get_json()
-    new_episode = Episodes(
-        title=data['title'],
-        description=data['description'],
-        audio_url=data['audio_url']
+    new_episode = Episode(
+        title=data["title"],
+        description=data["description"],
+        audio_url=data["audio_url"]
     )
     db.session.add(new_episode)
     db.session.commit()
     return jsonify({"message": "Episode added"}), 201
+
+@main.route("/episodes", methods=["GET"])
+def get_episodes():
+    episodes = Episode.query.all()
+    return jsonify([{
+        "id": ep.id,
+        "title": ep.title,
+        "description": ep.description,
+        "audio_url": ep.audio_url,
+        "date_posted": ep.date_posted
+    } for ep in episodes]), 200
+
+@main.route("/episodes/<int:id>", methods=["GET"])
+def get_episode(id):
+    episode = Episode.query.get_or_404(id)
+    return jsonify({
+        "id": episode.id,
+        "title": episode.title,
+        "description": episode.description,
+        "audio_url": episode.audio_url,
+        "date_posted": episode.date_posted
+    }), 200
+
+@main.route("/episodes/<int:id>", methods=["PUT"])
+def update_episode(id):
+    episode = Episode.query.get_or_404(id)
+    data = request.get_json()
+    episode.title = data.get("title", episode.title)
+    episode.description = data.get("description", episode.description)
+    episode.audio_url = data.get("audio_url", episode.audio_url)
+    db.session.commit()
+    return jsonify({"message": "Episode updated"}), 200
+
+@main.route("/episodes/<int:id>", methods=["DELETE"])
+def delete_episode(id):
+    episode = Episode.query.get_or_404(id)
+    db.session.delete(episode)
+    db.session.commit()
+    return jsonify({"message": "Episode deleted"}), 200
