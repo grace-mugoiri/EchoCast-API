00e7e5fe9fd95c4517748e62d264f1f8ed0a9143
Grace Mugoiri <gracemugoiri@gmail.com>
2025-04-15 20:14:22 +0300

    implement add episodes function and add requirements

--- app/routes.py
+++ app/routes.py
@@ -13,3 +13,16 @@ def get_episodes():
         "audio_url": e.audio_url,
         "date_posted": e.date_posted
     } for e in episodes])
+
+@main.route("/episodes", methods=["POST"])
+def add_episode():
+    data = request.get_json()
+    new_episode = Episodes(
+        title=data['title'],
+        description=data['description'],
+        audio_url=data['audio_url']
+    )
+    db.session.add(new_episode)
+    db.session.commit()
+    return jsonify({"message": "Episode added"}), 201
+    
